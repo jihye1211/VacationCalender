1034eefcc0324679b0a2b1eb228818e63638d174
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2023-04-10 01:05:35 +0900

    💄css 파일 추가
    test.css 파일에 기존 템플릿 css 파일 내용을 적용함.
    App.js에서 test.css import 추가
    -App.js 경로: bestvacation-spring/src/main/bestvacation-react/src/App.js
    *추가내용: 상단 import 추가
    import "./test.css"; (#3)
    -test.css 경로: bestvacation-spring/src/main/bestvacation-react/src/test.css

---
+++ bestvacation-spring/src/main/bestvacation-react/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "./test.css";
+
+function App() {
+    const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        // <div>
+        //     백엔드에서 가져온 데이터입니다 : {hello}
+        //     test
+        // </div>
+        <section id="hero" class="hero d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
+          <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
+          <div data-aos="fade-up" data-aos-delay="600">
+            <div class="text-center text-lg-start">
+              <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                <span>Get Started</span>
+                <i class="bi bi-arrow-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+          <img src="/img/hero-img.png" class="img-fluid" alt=""/>
+        </div>
+      </div>
+    </div>
+
+  </section>
+    )
+};
+
+
+export default App;
