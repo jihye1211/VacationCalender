d602ec99bbbbbaca7bc911a39cd81fc1e14fbf49
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2023-05-02 02:28:28 +0900

    ✨add: DatePicker
    DatePicker 기능 추가, 컴포넌트 작성

---
+++ bestvacation-spring/src/main/bestvacation-react/src/Calendar.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import "./test.css";
+
+function Calendar(props) {
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleChange = (date) => {
+        setSelectedDate(date);
+    };
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .catch(error => console.log(error))
+    }, []);
+    return (
+        // 캘린더 컴포넌트 구현 내용 작성
+        <DatePicker selected={selectedDate} onChange={handleChange} />
+    );
+}
+
+export default Calendar;
